bde92f547458c24424080e17d56f7fd0c5102aa4
efaby <efaby10@gmail.com>
2013-06-28 08:34:49 -0500

    Ajuste de direccion

--- Resources/parameter.js
+++ Resources/parameter.js
@@ -1,1 +1,8 @@
 exports.NAME_PAGE = 'Live On Stage';
+exports.DOMAIN = 'http://www.liveonstage.com/';
+exports.URL_FEED = 'index.php?option=com_mobile';
+exports.URL_CATEGORIES = 'index.php?option=com_mobile&task=categories';
+exports.URL_UPCOMING = 'index.php?option=com_mobile&task=events';
+exports.LIMIT = 10;
+exports.TOP_LIMIT = 50;
+exports.IMAGE_EVENT_DEFAULT='components/com_community/assets/event.png'
